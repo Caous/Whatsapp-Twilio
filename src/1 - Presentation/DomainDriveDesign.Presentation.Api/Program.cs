5007c25bb6849ac855073a03668878cced153154
caous <caous.g@gmail.com>
2025-02-11 20:19:12 -0300

    feat: cors

--- src/1 - Presentation/DomainDriveDesign.Presentation.Api/Program.cs
+++ src/1 - Presentation/DomainDriveDesign.Presentation.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://template-ui-dashboard-alpha.vercel.app/");
+        policy.WithOrigins("http://localhost:5173", "https://template-ui-dashboard-alpha.vercel.app");
         policy.AllowAnyHeader();
         policy.AllowAnyMethod();
     });
